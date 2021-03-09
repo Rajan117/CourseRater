049d8ac274824bcf6915950e7427042f2be2adaf
Joseph Mousa <2453578m@student.gla.ac.uk>
2021-03-09 23:51:04 +0200

    added basic model classes

--- CourseRate/models.py
+++ CourseRate/models.py
@@ -11,3 +11,30 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+class University(models.Model):
+
+    university_name = models.CharField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.university_name
+
+
+class Departments(models.Model):
+
+    university = models.ForeignKey(University, on_delete=models.CASCADE)
+    department_name = models.CharField(max_length=200, unique=True)
+
+
+    def __str__(self):
+        return self.department_name + "|" + university.university_name
+
+class Modules(models.Model):
+
+    department = models.ForeignKey(Departments, on_delete=models.CASCADE)
+    module_name = models.CharField(max_length=200, unique=True)
+
+    def __str__(self):
+        return (self.module_name + "|" + department.department_name + "|"
+                + department.university.university_name)
