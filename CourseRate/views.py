369a31bd2bdc20d1b908e2cfd3cca9e895616886
Rajan Rana <2596790r@student.gla.ac.uk>
2021-03-10 12:01:28 +0000

    Added: - Login view - Login template - Logout view - Conditional links in base.html - Login/logout functionality
    No errors detected.
    
    Rajan

--- CourseRate/views.py
+++ CourseRate/views.py
@@ -1,6 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from django.http import HttpResponse
 from CourseRate.forms import UserForm, UserProfileForm
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse
+from django.urls import reverse
+from django.shortcuts import redirect
 
 
 def home(request):
@@ -46,3 +50,32 @@ def register(request):
 
     return render(request, 'CourseRater/register.html',
                   context={'user_form': user_form, 'profile_form': profile_form, 'registered': registered})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return redirect(reverse('CourseRate:home'))
+            else:
+                return HttpResponse("Your account is disabled.")
+        else:
+            print(f"Invalid login details: {username}, {password}")
+            return HttpResponse("Invalid login details supplied.")
+
+    else:
+        return render(request, 'CourseRater/login.html')
+
+@login_required
+def user_logout(request):
+
+    logout(request)
+
+    return redirect(reverse('CourseRate:home'))
+
