4bd2924f2f21fbbc58001a84fdd2fb22246ffe9f
Roksana Staszynska <2450370S@student.gla.ac.uk>
2021-04-03 13:58:36 +0100

    Improved the look and functionality of the like/dislike buttons

--- static/js/module.js
+++ static/js/module.js
@@ -1,28 +1,39 @@
 $(document).ready(function() {
-
   $('.likeButton').on('click', function() {
-    this.innerHTML="Liked";
-    this.style.color = 'green';
-
-    var reviewIdVar = $(this).attr('data-reviewid');
-
-    $.get('/CourseRate/like_review/',
+    //  If the like button hasn't already been pressed
+    if (this.style.color != "green"){
+      this.style.color = 'green';
+      var likes = this.innerHTML;
+      var l = parseInt(likes);
+      l = l + 1;
+      likes = " " + l.toString();
+      this.innerHTML = likes;
+      var reviewIdVar = $(this).attr('data-reviewid');
+      $.get('/CourseRate/like_review/',
       {'review_id': reviewIdVar},
-      function(data) {
-        $('#like_count').html(data);
-      });
-  });
 
-  $('.dislikeButton').on('click', function() {
-    this.innerHTML="Disliked";
-    this.style.color = 'red';
-
-    var reviewIdVar = $(this).attr('data-reviewid');
-
-    $.get('/CourseRate/dislike_review/',
-      {'review_id': reviewIdVar},
       function(data) {
         $('#dislike_count').html(data);
       });
+    }
+  });
+
+  $('.dislikeButton').on('click', function() {
+    //  If the dislike button hasn't already been pressed
+    if (this.style.color != "red"){
+      this.style.color = 'red';
+      var dislikes = this.innerHTML;
+      var dl = parseInt(dislikes);
+      dl = dl + 1;
+      dislikes = " " + dl.toString();
+      this.innerHTML = dislikes;
+      var reviewIdVar = $(this).attr('data-reviewid');
+      $.get('/CourseRate/dislike_review/',
+      {'review_id': reviewIdVar},
+
+      function(data) {
+        $('#dislike_count').html(data);
+      });
+    }
   });
 });
